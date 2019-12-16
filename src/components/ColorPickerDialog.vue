b0e8451d42bc22775b0320959c86f65b2fe83fb2
Jan Lübben <jan.luebben@worldiety.com>
2019-12-16 19:59:58 +0100

    update

--- src/components/ColorPickerDialog.vue
+++ src/components/ColorPickerDialog.vue
@@ -1,16 +1,69 @@
 <template>
-
+    <v-dialog v-model="open">
+        <template v-slot:activator="{ on }">
+            <slot name="open" :on="on"></slot>
+        </template>
+        <v-card color="white">
+            <v-card-text>
+                <v-color-picker :value="color"></v-color-picker>
+                <v-combobox v-model="selectedAlignment" :items="alignments"/>
+                <v-checkbox v-model="showText" label="Text anzeigen"/>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn text @click="save">Ok</v-btn>
+                <v-btn text @click="open = false">Abbrechen</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
+  import { Component, Model, Vue, Watch } from 'vue-property-decorator';
+  import FileAttr from "@/components/classes/FileAttr";
 
   @Component
   export default class ColorPickerDialog extends Vue {
+    @Model()model: File;
 
+    open: boolean = false;
+
+    color: string = '';
+
+    alignments: string [] = ['left', 'center', 'top', 'bottom', 'right'];
+
+    selectedAlignment: string = '';
+
+    showText: boolean = false;
+
+    save(): void {
+      const attrs :FileAttr = new FileAttr();
+
+      attrs.align = this.selectedAlignment;
+
+      attrs.color = this.color;
+      attrs.showText = this.showText;
+
+      this.$store.commit('ADD_FILES_ATTRS', {key: this.model, val: attrs});
+
+      this.open = false;
+    }
+
+    @Watch('open')
+    load() {
+      if (this.open) {
+        const attrs :FileAttr = this.$store.getters.filesConfig.get(this.model);
+
+        if (attrs) {
+          this.color = attrs.color;
+          this.selectedAlignment = attrs.align;
+          this.showText = attrs.showText;
+        }
+      }
+    }
   }
 </script>
 
 <style scoped>
 
 </style>
+8
---
+++ src/components/classes/FileAttr.ts
@@ -0,0 +1,5 @@
+export default class FileAttr {
+  color!: string;
+  align!: string;
+  showText!: boolean;
+}
